77e5ddb96a93b41e4295d8bef132a4fca347cf8b
Ender Ahmet Yurt <enderyurt@gmail.com>
2016-04-13 19:54:07 +0300

    Third.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/webhook/', function(req, res, next) {
+if (req.query['hub.verify_token'] === 'nobetci') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
+
 module.exports = router;
