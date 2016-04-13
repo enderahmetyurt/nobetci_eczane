49717ff6a6936b79db6d8d912263cc6ebada1861
Ender Ahmet Yurt <enderyurt@gmail.com>
2016-04-13 19:56:26 +0300

    Forth.

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/webhook/', function(req, res, next) {
-if (req.query['hub.verify_token'] === 'nobetci') {
-    res.send(req.query['hub.challenge']);
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
   }
-  res.send('Error, wrong validation token');
+  res.sendStatus(200);
 });
 
 module.exports = router;
