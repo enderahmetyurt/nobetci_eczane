eb33b3d113795b2e3ab6fc3729fa0e3c04e694ce
Ender Ahmet Yurt <enderyurt@gmail.com>
2016-04-13 23:11:53 +0300

    Update.

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,50 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    request = require('request'),
+    router = express.Router(),
+    token = "CAAXls4rOxl4BAEHL3YPxuW4eDl0bn7ZAisOam8khhCTv7cZCn6ZAGKnMxJUwmUqotcqEVtkPgP6pfGTdCozjAzdZAVhL4lQ4npjDJ3AZAv440P2TDoaUm5TW5DheJizQuqWtWZB2CAMAv9V9H93je1zZCPd6z8q3RYizbjkdGBPFnkZABgp7ePmHjXdtZCJsTj5LejbZAtnG2WgwZDZD";
 
-/* GET home page. */
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.query['hub.verify_token'] === '<validation_token>') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
 });
 
-// router.get('/webhook/', function(req, res, next) {
-// if (req.query['hub.verify_token'] === 'nobetci') {
-//     res.send(req.query['hub.challenge']);
-//   }
-//   res.send('Error, wrong validation token');
-// });
-
-router.post('/webhook/', function (req, res) {
-  var token = "CAAXls4rOxl4BAKPJz4YEGP6rtcm0UR68PQaZApwIYlztoHztDINghAXVt0LGpefY2OZCkwTHDmEJ5DZBQdMgy4J3OBrTRyUtBOc2pD2qZAFlZA7eDRuI23dSVZCjbVTckfOuwMptqbo0yhheUwHCbxiVn3vtZBB3AB0TfOr4G5J5aN3tgS9ZA9nLplYnJuWH9ZAgRLIpw5oQf0QZDZD";
-  
-  function sendTextMessage(sender, text) {
-    console.log("IN method")
-    console.log(sender)
-    console.log(text)
-    messageData = {
-      text:text
-    }
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: token},
-      method: 'POST',
-      json: {
-        recipient: {id:sender},
-        message: messageData,
-      }
-    }, function(error, response, body) {
-      if (error) {
-        console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-      }
-    });
-  }
-
-  messaging_events = req.body.entry[0].messaging;
+router.post('/', function(req, res, next) {
+  var messaging_events = req.body.entry[0].messaging,
+      replayMessages = [];
   for (i = 0; i < messaging_events.length; i++) {
     event = req.body.entry[0].messaging[i];
     sender = event.sender.id;
     if (event.message && event.message.text) {
       text = event.message.text;
-      console.log(sender)
-      console.log(text)
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+
+      // アレコレしたいことをどうぞ
+      sendTextMessage(sender, text.substring(0, 200));
     }
   }
   res.sendStatus(200);
-  
 });
 
 module.exports = router;
